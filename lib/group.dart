1ff7358556a84d827359f1da61259415c0dbf66e
kzhdev <kun.zhao@tradingtechnologies.com>
2014-03-24 22:47:31 -0500

    Fix group _createSvgImpl

--- lib/group.dart
+++ lib/group.dart
@@ -5,18 +5,11 @@ class Group extends Node implements Container<Node> {
 
   Group([Map<String, dynamic> config = null]): super(config) {}
 
-  NodeImpl _createSvgImpl([bool isReflect = false]) {
+  NodeImpl _createSvgImpl() {
     SvgGroup impl = new SvgGroup(this);
     _children.forEach((node) {
-      if (isReflect && !node.draggable &&
-          !node.isListening && !(node is Container)) {
-        return;
-      }
-      node._impl = isReflect ? node.reflect() : node.createImpl(svg);
-      if (!(node is Container) ||
-          node.children.length > 0) {
-        impl.add(node._impl);
-      }
+      node._impl = node.createImpl(svg);
+      impl.add(node._impl);
     });
     return impl;
   }

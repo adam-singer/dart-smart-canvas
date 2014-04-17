4c45de70215da6311cff4bc8995fb8d0940ccc55
kzhdev <kun.zhao@tradingtechnologies.com>
2014-04-16 20:44:26 -0500

    Fix merge array

--- lib/utils/util.dart
+++ lib/utils/util.dart
@@ -1,7 +1,7 @@
 part of smartcanvas;
 
 String capitalize(String s) {
-  if (s.length > 0) {
+  if (s != null && s.length > 0) {
     if (s.length == 1) {
       return s[0].toUpperCase();
     }
@@ -11,6 +11,10 @@ String capitalize(String s) {
 }
 
 dynamic getValue(Map map, key, [defaultValue = null]) {
+  if (map == null) {
+    return defaultValue;
+  }
+
   var rt = map[key];
   return rt == null ? defaultValue : rt;
 }
@@ -28,13 +32,20 @@ Map merge(Map map1, Map map2, [Map map3 = null, Map map4 = null]) {
         if (v is Map) {
           rt[k] = merge(rt[k], map[k]);
         } else if (v is Iterable){
-          for (int i = 0; i < v.length && i < rt[k].length; i++) {
+          int i = 0;
+          List listLeft = rt[k];
+          for (; i < v.length && i < rt[k].length; i++) {
             if (v[i] is Map) {
-              rt[k][i] = merge(rt[k][i], v[i]);
+              listLeft[i] = merge(listLeft[i], v[i]);
             } else {
-              rt[k][i] = v[i];
+              listLeft[i] = v[i];
             }
           }
+
+          for (; i < v.length; i++) {
+            listLeft.add(v[i]);
+          }
+
         } else {
           rt[k] = v;
         }
@@ -44,7 +55,10 @@ Map merge(Map map1, Map map2, [Map map3 = null, Map map4 = null]) {
     });
   }
 
-  _merge(map2);
+  if (map2 != null) {
+    _merge(map2);
+  }
+
   if (map3 != null) {
     _merge(map3);
   }
